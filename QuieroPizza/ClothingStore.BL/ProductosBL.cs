4c450b021717557d3828a391a989d517f8632e3b
IrisVH <ivhernandez@unah.hn>
2021-09-26 21:10:57 -0600

    Agregando Entity Framework

--- QuieroPizza/ClothingStore.BL/ProductosBL.cs
+++ QuieroPizza/ClothingStore.BL/ProductosBL.cs
@@ -8,37 +8,24 @@ namespace ClothingStore.BL
 {
     public class ProductosBL
     {
+
         Contexto _contexto;
+        public List<Producto> ListadeProductos { get; set; }
+
         public ProductosBL()
         {
             _contexto = new Contexto();
-        }
+            ListadeProductos = new List<Producto>();
+           
+           
+
+    }
 
         public List<Producto> ObtenerProductos()
         {
-            _contexto.Productos.ToList();
-
-            var producto1 = new Producto();
-            producto1.Id = 1;
-            producto1.Descripcion = "Camisa tipo Polo";
-            producto1.Precio = 350;
-
-            var producto2 = new Producto();
-            producto2.Id = 2;
-            producto2.Descripcion = "Vestido";
-            producto2.Precio = 650;
-
-            var producto3 = new Producto();
-            producto3.Id = 3;
-            producto3.Descripcion = "Pantalon Gean";
-            producto3.Precio = 730;
-
-            var listadeProductos = new List<Producto>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
-
-            return listadeProductos;
+            ListadeProductos = _contexto.Productos.ToList();
+            return  ListadeProductos;
+         
         }
     }
 }
