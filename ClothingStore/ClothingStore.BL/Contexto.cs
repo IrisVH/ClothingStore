d0f70b01c0f668993a74b99670b3092b842e36d5
IrisVH <ivhernandez@unah.hn>
2021-10-08 17:07:19 -0600

    Agragando cambios

--- ClothingStore/ClothingStore.BL/Contexto.cs
+++ ClothingStore/ClothingStore.BL/Contexto.cs
@@ -21,5 +21,6 @@ namespace ClothingStore.BL
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
         public DbSet<Producto> Productos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
     }
 }
