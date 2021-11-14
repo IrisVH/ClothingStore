3f751b224ba16993b66ac08c26facc8036cf988b
IrisVH <ivhernandez@unah.hn>
2021-11-14 14:20:48 -0600

    Agregando template public a nuestro sitio web publico

--- ClothingStore/ClothingStore.Web/Controllers/HomeController.cs
+++ ClothingStore/ClothingStore.Web/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ClothingStore.BL;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,7 +13,12 @@ namespace ClothingStore.Web.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            var productosBL = new ProductosBL();
+            var listadeproductos = productosBL.ObtenerProductosActivos();
+
+            ViewBag.adminWebsiteUrl = 
+                ConfigurationManager.AppSettings["adminWebsiteUrl"];
+            return View(listadeproductos);
         }
     }
 }
